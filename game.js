1c57da5d9c77d7f276c5c6cb1494ecff49c8d79c
Sebastián Carabali <sebastiancc@unicauca.edu.co>
2020-06-01 23:04:39 -0500

    Se crea la funcionalidad de pintar la secuencia de colores

--- game.js
+++ game.js
@@ -33,9 +33,55 @@ function $(selector, container) {
 })();
 
 (function () {
-  var _ = (self.SimonView = function () {});
+  var _ = (self.SimonView = function () {
+    this.init();
+  });
 
-  _.prototype = {};
+  _.prototype = {
+    init: function () {
+      this.getColors();
+      this.addClickEvents();
+    },
+    getColors: function () {
+      this.colors = [
+        $(".btn.red"),
+        $(".btn.green"),
+        $(".btn.yellow"),
+        $(".btn.blue"),
+      ];
+    },
+    addClickEvents: function () {
+      this.colors.forEach((item) => {
+        item.addEventListener("click", (ev) => {
+          console.log(ev.target);
+          console.log(this);
+        });
+      });
+    },
+    printSequence: async function (sequence) {
+      for (let i = 0; i < sequence.length; i++) {
+        await this.buttonOn(this.colors[i]).then((color) => {
+          return this.buttonOff(color);
+        });
+      }
+    },
+    buttonOn: function (color) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          color.classList.add("light");
+          resolve(color);
+        }, 1000);
+      });
+    },
+    buttonOff: function (color) {
+      return new Promise((res, rej) => {
+        setTimeout(() => {
+          color.classList.remove("light");
+          res(color);
+        }, 1100);
+      });
+    },
+  };
 })();
 
 (function () {
